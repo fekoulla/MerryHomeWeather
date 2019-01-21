fb7420c38b997e79c508e6bd9618d7b1eb5ac973
MagicRR <r.richard.94@orange.fr>
2019-01-21 16:38:50 +0100

    vitesse du vent

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 
+
 <?php
 
-$json = file_get_contents('http://www.prevision-meteo.ch/services/json/genk');
+$json = file_get_contents('http://www.prevision-meteo.ch/services/json/antibes');
 $json = json_decode($json);
 
-echo $json->city_info->name;
+?>
+
+ <img src="<?php echo $json->current_condition->icon; ?>" width="45" height="45" />
+ <br/>
+ <br/>
+
+<?php
+
+echo "Il fait " .$json->current_condition->tmp. " degrés à " .$json->city_info->name. ".";
+echo "<br/>";
+echo "La vitesse du vent est de ". $json->current_condition->wnd_spd . " km/h.";
+// echo
+// $json->current_condition->wnd_dir
 
  ?>
 
- <img src="<?php echo $json->current_condition->icon; ?>" width="45"
- height="45" />
+<hr>
 
-<div style="width:510px;color:#000;border:1px solid #F2F2F2;">
+
+<!-- <div style="width:510px;color:#000;border:1px solid #F2F2F2;"> -->
 Prévisions complètes pour <?php echo $json->city_info->name; ?>
-</div>
