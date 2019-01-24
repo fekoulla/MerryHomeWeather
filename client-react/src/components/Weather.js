0612f0968efcba9770051fa55bececf662731d9c
Sébastien MINES <sebastien.mines@recommerce.com>
2019-01-24 17:12:27 +0100

    MerryHomeWeather : version stable 0.1.0

--- client-react/src/components/Weather.js
+++ client-react/src/components/Weather.js
@@ -1,16 +1,87 @@
-import React from 'react'
-import PluginActionButton from "./PluginActionButton"
+import React, { PropTypes, Component } from 'react'
+import {isConfigured} from '../utils/authservice'
+import SpeechRecognition from 'react-speech-recognition'
+import { Form, FormGroup,FormControl, ControlLabel, Button, Glyphicon } from 'react-bootstrap'
+import {subscribeToEvent, emitEvent, sendRequest, getExpressions} from '../utils/serverhome-api'
+import {searchRequest} from '../utils/voice-helper'
 import './Weather.css';
 
+
 class Weather extends React.Component {
-    render() {
-        var props = this.props;
-        return (
-            <div className='plugincontent plugin-weather'>
-                <h1>TEST</h1>
-            </div>
-        );
-    }
-};
+
+  constructor(props){
+      super(props);
+      this.state = { searchValue: "",
+                     shortResult: "",
+                     expressions:[],
+                     isTable : false,
+                     searchResult: null };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (event) {
+     this.setState({
+          searchValue: event.target.value
+      });
+  }
+
+  handleSubmit (event) {
+      console.log("emit event weathersearch : "+this.state.searchValue);
+      emitEvent("weathersearch", this.state.searchValue);
+      var self= this;
+
+      var test = searchRequest(this.state.searchValue, this.state.expressions);
+      console.log(test);
+      sendRequest(test.plugin, test.action, test.data).then((data)=>{
+          if(data.resultText){
+              var utterThis = new SpeechSynthesisUtterance(data.resultText);
+              utterThis.lang = 'fr-FR';
+              console.log({"response":data.resultText});
+              window.speechSynthesis.speak(utterThis);
+              self.setState({
+                  shortResult: data.resultText
+              });
+          }
+      });
+      if(event)
+          event.preventDefault();
+  }
+
+  componentDidMount(){
+      if(!isConfigured()) return;
+      var self= this;
+      getExpressions().then((expressions)=>{
+          self.setState({"expressions": expressions});
+      });
+
+  }
+
+  render() {
+      var result = this.state.searchResult ?
+                      (this.state.isTable ?
+                      <table dangerouslySetInnerHTML={{ __html: this.state.searchResult }} /> :
+                      <div dangerouslySetInnerHTML={{ __html: this.state.searchResult }} />)
+                  : "";
+      return (
+          <div className='plugincontent plugin-weather'>
+              <Form onSubmit={this.handleSubmit} inline>
+                  <FormGroup controlId="formInlineName">
+                      <ControlLabel>Search</ControlLabel>{' '}
+                      <FormControl type="text" placeholder="terms" value={this.state.searchValue} onChange={this.handleChange} />
+                  </FormGroup>{' '}
+                  <Button type="submit"><Glyphicon glyph="search" /> </Button>
+              </Form>
+              <div className="shortResult">
+                  <cite>{this.state.shortResult.condition}</cite>
+              </div>
+              <div className="result">
+                  {this.state.shortResult.condition}
+              </div>
+          </div>
+      );
+  }
+
+}
 
 export default Weather;
--- serverhome/plugins/weather/controller.js
+++ serverhome/plugins/weather/controller.js
@@ -1,16 +1,42 @@
+
 const request = require('sync-request');
+const rp = require('request-promise');
+const $ = require('cheerio');
+const url = 'http://www.prevision-meteo.ch/services/json/';
 
 class WeatherController {
 
     constructor(io){
             this.io = io;
+            io.sockets.on('connection', function(socket){
+                socket.on('weathersearch', function(searchvalue){
+                    console.log("wearthersearch "+searchvalue);
+                    var isTable=true;
+                    rp(url+searchvalue)
+                    .then(function(html) {
+                        var title= $('.firstHeading', html).text();
+                        var infos = $('.infobox_v2', html).html();
+                        if(!infos){
+                            infos= $('.infobox_v3', html).html();
+                            if(!infos){
+                                infos=$('.mw-parser-output', html).html();
+                                isTable= false;
+                            }
+                        }
+                        socket.emit('weatherresult', {title: title, infos: infos, isTable: isTable});
+                    })
+                    .catch(function(err) {
+                      //handle error
+                    });
+                });
+            });
     }
 
     getView(req, res){
-        var dataView = require("./view");
+        var dataView = {
+            "type" : "Weather"
+        };
         res.end(JSON.stringify(dataView));
-        // var path = require("path");
-        // res.sendFile(path.join(__dirname, '../client-react/src/', 'view.js'));
     }
 
     postAction(req, res){
@@ -24,6 +50,7 @@ class WeatherController {
 
                 //Enlève le _ du searchDate
                 var date = req.body.searchDate;
+                console.log(date);
 
                 if(date == 'maintenant'){
                   response = response.current_condition;
@@ -43,6 +70,8 @@ class WeatherController {
                   response = "Nous n'avons pas d'informations concernant cette date"
                 }
 
+                console.log(response);
+
                 if(!response){
                     res.end(JSON.stringify({resultText: "je n'ai pas d'informations"}));
                 }else{
@@ -55,5 +84,4 @@ class WeatherController {
         }
     }
 }
-
 module.exports = WeatherController;
